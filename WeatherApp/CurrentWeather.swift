86b8a00f60880a730b790e81c620b221a05dd6ef
Aleksandr Kalinin <lawejr@gmail.com>
2017-03-29 18:00:21 +0500

    add weather icon setup enum

---
+++ WeatherApp/CurrentWeather.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentWeather.swift
+//  WeatherApp
+//
+//  Created by Aleksandr Kalinin on 29.03.17.
+//  Copyright © 2017 Aleksandr Kalinin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct CurrentWeather {
+  let temperature: Double
+  let appearentTemperature: Double
+  let humidity: Double
+  let pressure: Double
+  let icon: UIImage
+}
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var locationLabel: UILabel!
+  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet weak var pressureLabel: UILabel!
+  @IBOutlet weak var humidityLabel: UILabel!
+  @IBOutlet weak var temperatureLabel: UILabel!
+  @IBOutlet weak var appearentTemperatureLabel: UILabel!
+  @IBOutlet weak var refreshBtn: UIButton!
+  
+  
+  @IBAction func refreshBtnTapped(_ sender: UIButton) {
+    
+  }
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
